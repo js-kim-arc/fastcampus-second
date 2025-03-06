0ce706300e7857220d3aa0b0fecc7cbe8d5d2453
junseong Kim <rlawnstj90@naver.com>
2025-03-06 18:30:50 +0900

    #12-리포지터리에 querydsl 관련 넣어놓기
    관련 기능 build gradle 삽입 및 기본 data.sql 완성

--- projectboard/src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ projectboard/src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,32 @@
 package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title,root.hashtag,root.createdAt,root.createdBy);
+        // 기본 바인딩 설정 (예: 문자열은 대소문자 무시 검색)
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first((path, value) -> path.eq(value));
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
