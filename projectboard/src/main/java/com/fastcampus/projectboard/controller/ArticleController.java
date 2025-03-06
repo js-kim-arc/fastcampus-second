359f102f7f9f2dcf4fab111475dd0c0104e91025
junseong Kim <rlawnstj90@naver.com>
2025-03-06 20:45:28 +0900

    #12- 리팩토링
    전반적으로 수정하면서 이상했던거 수정하였습니다.
    - 추가 기능으로는  api 관련 테스트 임시 작성 완료하였습니다.

--- projectboard/src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
+++ projectboard/src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -1,2 +1,8 @@
-package com.fastcampus.projectboard.controller;public class ArticleController {
+package com.fastcampus.projectboard.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
 }
--- projectboard/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ projectboard/src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -1,4 +1,85 @@
+package com.fastcampus.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+@DisplayName("View 컨트롤러 - 게시글 ")
+@WebMvcTest
+@AutoConfigureMockMvc
 class ArticleControllerTest {
-  
+    private final MockMvc mvc;
+
+    ArticleControllerTest(@Autowired MockMvc mvc)
+    {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][Get]- 게시글 리스트 페이지 - 정상 호출 ")
+    @Test
+    void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
+        //given
+        //when
+        mvc.perform(get("/articles"))
+           .andExpect(status().isOk())
+           .andExpect(content().contentType(MediaType.TEXT_HTML))
+           .andExpect(model().attributeExists("articles"));
+        //Then
+
+    }
+
+    @DisplayName("[view][Get]- 게시글 상세 페이지 테스트 - 정상 호출 ")
+    @Test
+    void givenNothing_whenRequestingArticlesDetailView_thenReturnsArticlesDetailPage() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/articles/1"))
+           .andExpect(status().isOk())
+           .andExpect(content().contentType(MediaType.TEXT_HTML))
+           .andExpect(model().attributeExists("articles/1"));
+        //Then
+
+    }
+
+    @DisplayName("[view][Get]- 게시글 검색 페이지 테스트 - 정상 호출 ")
+    @Test
+    void givenNothing_whenRequestingArticlesSearchView_thenReturnsArticlesSearchView() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/articles/search"))
+           .andExpect(status().isOk())
+           .andExpect(content().contentType(MediaType.TEXT_HTML))
+           .andExpect(model().attributeExists("articles/1"));
+        //Then
+
+    }
+
+    @DisplayName("[view][Get]- 게시글 해시태그 검색 페이지 테스트 - 정상 호출 ")
+    @Test
+    void givenNothing_whenRequestingArticlesHashtagSearchView_thenReturnsArticlesHashtagView() throws Exception {
+        //given
+
+        //when
+        mvc.perform(get("/articles/search-hashtag"))
+           .andExpect(status().isOk())
+           .andExpect(content().contentType(MediaType.TEXT_HTML))
+           .andExpect(model().attributeExists("articles/1"));
+        //Then
+
+    }
+
+
+
 }
+
